ed61641e3a21b20ee9cfff337c7a8b0c47bfb13d
marianasindeaux <mariana.sindeaux@hotmail.com>
2026-01-31 14:33:01 -0300

    Add files via upload

---
+++ Aula 3/exercicio_02.py
@@ -0,0 +1,29 @@
+#ATIVIDADE 2
+#CRIAR UMA CALCULADORA COM A FUNÇÃO INPUT (print())
+#PRECISA TER AS 4 OPERAÇÕES
+
+
+print('*******' * 10)
+print('Calculadora Python')
+print()
+n1 = float(input('Digite primeiro valor: '))
+n2 = float(input('Digite segundo valor: '))
+
+soma = n1 + n2
+subtração = n1 - n2
+multiplicação = n1 * n2
+divisão = n1 / n2
+
+print('......' *5)
+
+print('Soma: ', soma)
+print()
+print('Subtração: ', subtração)
+print()
+print('Multiplicação: ', multiplicação)
+print()
+print('Divisão: ', divisão)
+
+ 
+
+
