c23c3aec427cc3f05f12208b6b88c1d4de5f0e02
marianasindeaux <mariana.sindeaux@hotmail.com>
2026-01-31 15:48:31 -0300

    Add files via upload

---
+++ aula_4.py
@@ -0,0 +1,33 @@
+# ATIVIDADES:
+#1 - Crie um programa para efetuar a leitura de um número inteiro e apresentar o resultado do quadrado deste número.
+
+numero = 10
+print(numero**2)
+print('*******'*10)
+#2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome. Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+
+nome = 'Mariana'
+sobrenome = 'Sindeaux'
+
+print(nome, sobrenome)
+print('*******'*10)
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números como strings e exiba o resultado.
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digete um numero: '))
+print()
+print(n1 , n2)
+print('*******'*10)
+#4 - Crie uma variável para armazenar a palavra "Python". Em seguida, adicione um número inteiro ao final da palavra usando a concatenação e exiba o resultado.
+
+p1 = 'Python'
+print(p1 , 10)
+print('*******'*10)
+#5 - Declare uma variável contendo uma frase. Em seguida, peça ao usuário para digitar uma palavra e concatene essa palavra no final da frase. Exiba o resultado. 
+
+frase = 'Otimo final de semana, '
+
+palavra1 = input('Digite seu nome: ')
+
+print( frase, palavra1)
+print('*******'*10)
